dbfe575a5d9a72a6b4b40db3ef3bd6b23ae7bf5f
Jaydip2001 <jaydip2801@gmail.com>
2026-03-01 11:43:46 +0530

    fixed safari mobile

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/jaydip_darji_portfolio",
   build: {
     target: "es2015"
   }
